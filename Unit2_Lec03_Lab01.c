8fb918679f4acbbc0a026c84cfa57b4b156fa9cd
Barakat Ibrahim <barakat.ibhasan@gmail.com>
2022-10-22 17:23:49 +0200

    Unit 2 Lecture 03 Lab 01 Initial Release

---
+++ Unit2_Lec03_Lab01.c
@@ -0,0 +1,20 @@
+/*
+ * Created By Barakat
+ * 22-10-2022
+ * Unit 2 - C Programming
+ * Lecture 3
+ * Lab 01
+ * Data types
+ */
+
+#include "stdio.h"
+int main(){
+	int x = 5;
+	float y = 2.0;
+	if(x/y ==2)
+		printf(" int/float >>> int \n");
+	else if (x/y == 2.5)
+		printf (" int/float >>> float \n");
+
+	return 0;
+}
